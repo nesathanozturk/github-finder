d8f22c1d8c87b6ae14e522af0df9625608cf3eb2
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-05-15 13:01:07 +0300

    style: changed title size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,11 @@ import { HiSun } from "react-icons/hi";
 const Header = () => {
   return (
     <header className="flex justify-between items-center text-white">
-      <h1 className="text-xl font-semibold">devfinder</h1>
-      <div className="flex justify-center items-center gap-1">
-        <span className="uppercase text-xs">Light</span>
+      <h1 className="text-2xl font-semibold">devfinder</h1>
+      <div className="flex justify-center items-center gap-1.5">
+        <span className="uppercase text-xs font-semibold tracking-widest">
+          Light
+        </span>
         <span>
           <HiSun size={18} />
         </span>
