9430475b4139041ba29d57ba023e1b14a40b9acd
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-05-15 13:56:55 +0300

    feat: added api

--- src/store/slices/profileSlices.js
+++ src/store/slices/profileSlices.js
@@ -1,11 +1,42 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const BASE_URL = "https://api.github.com/users";
+
+export const getUser = createAsyncThunk("getUser", async (searchValue) => {
+  const res = await axios.get(`${BASE_URL}${searchValue}`);
+  const data = await res.data;
+  return data;
+});
 
 export const profileSlice = createSlice({
   name: "profile",
   initialState: {
+    users: [],
     searchValue: "",
+    loading: false,
+    error: "",
   },
-  reducers: {},
+  reducers: {
+    setSearchValue: (state, action) => {
+      state.searchValue = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getUser.pending, (state, action) => {
+      state.loading = true;
+    });
+    builder.addCase(getUser.fulfilled, (state, action) => {
+      state.users = action.payload;
+      state.loading = false;
+    });
+    builder.addCase(getUser.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.error.message;
+    });
+  },
 });
 
 export default profileSlice.reducer;
+
+export const { setSearchValue } = profileSlice.actions;
